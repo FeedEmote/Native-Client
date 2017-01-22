496c7b064eca288942542b90ae6fec1845678ced
Maas Lalani <maas@live.ca>
2017-01-22 18:03:48 -0500

    Clean up recording screen

--- App/screens/recording.js
+++ App/screens/recording.js
@@ -2,17 +2,18 @@ import React, { Component, TouchableOpacity } from 'react'
 import { View, Text } from 'react-native'
 
 import Camera from 'react-native-camera'
-// import fs from 'react-native-fs'
+
+// File Upload
 const FileUpload = require('NativeModules').FileUpload
 
-let interval
+const server = 'http://142.150.208.170'
+let interval = null
 
 export default class Recording extends Component {
   constructor(props) {
     super(props)
     this.state = {
       running: false,
-      presentationId: '',
       uid: 'NHHePgE5P4MTRUjcY8EmeNXvoys1',
       pr_id: ''
     }
@@ -20,9 +21,10 @@ export default class Recording extends Component {
 
   _takePicture() {
     this.camera.capture()
-      .then((data) => {
-        let obj = {
-          uploadUrl: 'http://142.150.208.170/upload/',
+      .then((image) => {
+
+        let request = {
+          uploadUrl: `${server}/upload/`,
           method: 'POST',
           headers: {
             'Accept': 'application/json',
@@ -34,43 +36,49 @@ export default class Recording extends Component {
           files: [
             {
               filename: 'IMG.jpg',
-              filepath: data.path
+              filepath: image.path
             }
           ]
         }
-        FileUpload.upload(obj, function (err, result) {
-          console.log('upload:', err, result);
-        })
+
+        FileUpload.upload(request, (err, result) => console.log('upload:', err, result))
+
       })
   }
 
   _startButton() {
     return (
-      <View style={styles.startCapture}>
-        <Text onPress={() => {
-          this.setState({ running: true })
-          fetch(`http://142.150.208.170/addpr?userid=${this.state.uid}`)
-            .then((response) => response.json())
-            .then((responseJSON) => this.setState({ pr_id: responseJSON.pr_key }))
-            .catch((error) => console.log(error))
-          interval = setInterval(() => {
-            this._takePicture()
-          }, 5000)
-        } } style={styles.startCaptureText}>
+      <View style={[styles.startCapture, styles.captureButton]}>
+
+        <Text
+          style={styles.startCaptureText}
+          onPress={() => {
+            this.setState({ running: true })
+
+            interval = setInterval(() => this._takePicture(), 5000)
+
+            fetch(`${server}/addpr?userid=${this.state.uid}`)
+              .then((response) => response.json())
+              .then((response) => this.setState({ pr_id: responseJSON.pr_key }))
+              .catch((error) => console.log(error))
+          }
+        }>
           START
         </Text>
+
       </View>
     )
   }
 
   _stopButton() {
     return (
-      <View style={styles.stopCapture}>
-        <Text onPress={() => {
-          this.setState({ running: false })
-          clearInterval(interval)
-          fetch(`http://142.150.208.170/endpr?userid=${this.state.uid}`)
-        }
+      <View style={[styles.stopCapture, styles.captureButton]}>
+        <Text
+          onPress={() => {
+            fetch(`${server}/endpr?userid=${this.state.uid}`)
+            this.setState({ running: false })
+            clearInterval(interval)
+          }
         }>
           STOP
         </Text>
@@ -81,21 +89,22 @@ export default class Recording extends Component {
   render() {
     return (
       <View style={[styles.container, styles.camera]}>
+
         <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          } }
+          ref={(camera) => this.camera = camera}
           captureQuality={Camera.constants.CaptureQuality.high}
           captureTarget={Camera.constants.CaptureTarget.temp}
-          // orientation={Camera.constants.Orientation.landscapeLeft}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill} >
 
           {
-            this.state.running ? this._stopButton() : this._startButton()
+            this.state.running
+              ? this._stopButton()
+              : this._startButton()
           }
 
         </Camera>
+
       </View>
     )
   }
